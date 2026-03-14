65d40d9a97da5cde7134a34ba873d8d71025d9e6
Dauquan007 <lequynhmar@gmail.com>
2026-03-14 14:13:47 +0700

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,6 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://nrjbwwdqerrpmqznnrwa.supabase.co"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const supabaseKey = "sb_publishable_ZgpTeJbDpTtC09iIOX9PEQ_V4MFrzzf"
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-)
+export const supabase = createClient(supabaseUrl, supabaseKey)
